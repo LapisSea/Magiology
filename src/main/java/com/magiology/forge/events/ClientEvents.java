ff78371cd0b759d8bbfcfec944756919e04bbabe
LapisSea <toni.tube.you@gmail.com>
2016-12-11 14:27:01 +0100

    update

--- src/main/java/com/magiology/forge/events/ClientEvents.java
+++ src/main/java/com/magiology/forge/events/ClientEvents.java
@@ -2,20 +2,22 @@ package com.magiology.forge.events;
 
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@EventBusSubscriber
 public class ClientEvents{
 
 	@SubscribeEvent
-	public void textureStitchPre(TextureStitchEvent.Pre e){
+	public static void textureStitchPre(TextureStitchEvent.Pre e){
 		//e.getMap().registerSprite(new ResourceLocationM(""));
 	}
 	@SubscribeEvent
-	public void textureStitchPost(TextureStitchEvent.Post e){
+	public static void textureStitchPost(TextureStitchEvent.Post e){
 		
 	}
 	@SubscribeEvent
-	public void modelBake(ModelBakeEvent e){
+	public static void modelBake(ModelBakeEvent e){
 //		e.getModelLoader().
 	}
 	
